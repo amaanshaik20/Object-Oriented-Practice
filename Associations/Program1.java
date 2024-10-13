677d6d945a83e3d8edfc2277cc2c7eb729162bfd
Mohammed Amaan <smdamaan20@gmail.com>
2024-10-13 22:23:20 +0530

    aaa

---
+++ Associations/Program1.java
@@ -0,0 +1,76 @@
+//aggregate class
+class Charger {
+    private String brand;
+    private float voltage;
+
+    public Charger(String brand, float voltage) {
+        this.brand = brand;
+        this.voltage = voltage;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public float getVoltage() {
+        return voltage;
+    }
+}
+
+// composition class
+class Os {
+    private String name;
+    private int size;
+
+    public Os(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
+
+// Mobile class
+class Mobile {
+    // composition
+    Os os = new Os("windows", 4);
+
+    // aggregation
+    public void hasA(Charger c) {
+        System.out.println(c.getBrand());
+        System.out.println(c.getVoltage());
+    }
+
+}
+
+public class Program1 {
+    public static void main(String[] args) {
+        Charger c = new Charger("samsung", 60.0f);
+
+        Mobile m = new Mobile();
+        m.hasA(c);
+        String brand = c.getBrand();
+        float voltage = c.getVoltage();
+        System.out.println(brand);
+        System.out.println(voltage);
+
+        String chargerName = m.os.getName();
+        int chargerSize = m.os.getSize();
+        System.out.println(chargerName);
+        System.out.println(chargerSize);
+
+        m = null;
+        // m.hasA(c);
+        System.out.println(c.getBrand());
+        System.out.println(c.getVoltage());
+        System.out.println(os.getName());
+
+    }
+
+}
---
+++ Associations/Program2.java
@@ -0,0 +1,116 @@
+class Heart {
+    private int weight;
+    private int bpm;
+
+    public Heart(int weight, int bpm) {
+        this.weight = weight;
+        this.bpm = bpm;
+    }
+
+    public int getBpm() {
+        return bpm;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
+
+class Brain {
+    private int weight;
+    private String color;
+
+    public Brain(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+}
+
+class Bike {
+    private String brand;
+    private int mileage;
+
+    public Bike(String brand, int mileage) {
+        this.brand = brand;
+        this.mileage = mileage;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+}
+
+class Book {
+    private String name;
+    private String author;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
+
+class Human {
+    Heart h = new Heart(250, 72);
+    Brain b = new Brain(1250, "White");
+
+    public void hasABike(Bike bike) {
+        System.out.println(bike.getBrand());
+        System.out.println(bike.getMileage());
+    }
+
+    public void hasABook(Book book) {
+        System.out.println(book.getName());
+        System.out.println(book.getAuthor());
+    }
+}
+
+public class Program2 {
+    public static void main(String[] args) {
+        Bike bike = new Bike("tvs", 45);
+        Book book = new Book("Java", "JAMES Gosling");
+
+        Human human = new Human();
+        human.hasABike(bike);
+        System.out.println();
+        human.hasABook(book);
+        System.out.println();
+
+        System.out.println(human.h.getWeight());
+        System.out.println(human.h.getBpm());
+        System.out.println();
+        System.out.println(human.b.getWeight());
+        System.out.println(human.b.getColor());
+        System.out.println();
+        human = null;
+
+        System.out.println(bike.getBrand());
+        System.out.println(book.getName());
+
+        System.out.println(human.h.getWeight());
+
+    }
+}
