705ff6318395f6b0f3c2e3153fe93da8c5527d63
Mohammed Amaan <smdamaan20@gmail.com>
2024-11-13 01:12:44 +0530

    multithreading

--- Multithreading/Program2.java
+++ Multithreading/Program2.java
@@ -6,15 +6,19 @@ import java.util.Scanner;
 // Program means if a file is in RAM
 // Thread is defined as an independent instructions who will execute independently and is part of process.
 // Multithreading is used to utilize CPU time efficiently
-// in Multithreading, concurrent execution occurs.
 
 // There are two ways to implement Multithreading 
 // 1-> Extending the Thread class
 // 2-> Implementing the Runnable interface
 
-// The First Method is ----->
+// The Second Method is ----->
+// The reason of crearing a second method that 
+// is by implementing a runnable interface is that
+//  in first method if a class also extends some other 
+// class other than Thread class, Its not possible 
+// because of diamond problem or multiple inheritance
 
-class Demo1 extends Thread {
+class Demo1 implements Runnable {
     @Override
     public void run() {
         Scanner sc = new Scanner(System.in);
@@ -27,7 +31,7 @@ class Demo1 extends Thread {
     }
 }
 
-class Demo2 extends Thread {
+class Demo2 implements Runnable {
     @Override
     public void run() {
         for (int i = 65; i < 70; i++) {
@@ -42,7 +46,7 @@ class Demo2 extends Thread {
     }
 }
 
-class Demo3 extends Thread {
+class Demo3 implements Runnable {
     @Override
     public void run() {
         for (int i = 1; i <= 10; i++) {
@@ -57,19 +61,19 @@ class Demo3 extends Thread {
     }
 }
 
-public class Program1 {
+public class Program2 {
     public static void main(String[] args) {
         Demo1 d1 = new Demo1();
         Demo2 d2 = new Demo2();
         Demo3 d3 = new Demo3();
 
-        // d1.run();
-        // d2.run();
-        // d3.run();
+        Thread t1 = new Thread(d1);
+        Thread t2 = new Thread(d2);
+        Thread t3 = new Thread(d3);
 
-        d1.start();
-        d2.start();
-        d3.start();
+        t1.start();
+        t2.start();
+        t3.start();
 
     }
 }
